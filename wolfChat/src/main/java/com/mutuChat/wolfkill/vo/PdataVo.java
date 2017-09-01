45535d7fb31a9dff717395f1cd4c54a676b675c1
Micro-Rice <635435791@qq.com>
2017-09-01 20:44:37 +0800

    增加Vo

---
+++ wolfChat/src/main/java/com/mutuChat/wolfkill/vo/PdataVo.java
@@ -0,0 +1,46 @@
+package com.mutuChat.wolfkill.vo;
+
+public class PdataVo {
+	private String playerName;
+	private int playerId;
+	private int roleIndex;
+	private int winSign;
+	private int Mvp;
+	private int achiveIndex;
+	public String getPlayerName() {
+		return playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	public int getPlayerId() {
+		return playerId;
+	}
+	public void setPlayerId(int playerId) {
+		this.playerId = playerId;
+	}
+	public int getRoleIndex() {
+		return roleIndex;
+	}
+	public void setRoleIndex(int roleIndex) {
+		this.roleIndex = roleIndex;
+	}
+	public int getWinSign() {
+		return winSign;
+	}
+	public void setWinSign(int winSign) {
+		this.winSign = winSign;
+	}
+	public int getMvp() {
+		return Mvp;
+	}
+	public void setMvp(int mvp) {
+		Mvp = mvp;
+	}
+	public int getAchiveIndex() {
+		return achiveIndex;
+	}
+	public void setAchiveIndex(int achiveIndex) {
+		this.achiveIndex = achiveIndex;
+	}
+}
